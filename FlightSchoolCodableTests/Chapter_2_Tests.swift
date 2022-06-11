3ad620edaaa311eb309678de8df936ced583eafb
Rudi Farkas <rudi.farkas@gmail.com>
2022-06-11 20:48:23 +0200

    add Chapter_2_Tests.swift; add .gitignore, .pdf

---
+++ FlightSchoolCodableTests/Chapter_2_Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter_2_Tests.swift
+//  Chapter_2_Tests
+//
+//  Created by Rudolf Farkas on 11.06.22.
+//
+
+@testable import FlightSchoolCodable
+import RudifaUtilPkg
+import XCTest
+
+class Chapter_2_Tests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func test_HoldingPatterns() throws {}
+}
