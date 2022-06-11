9e5e2e242a3aa421eb1aa7b2495b5cdece544bf7
Rudi Farkas <rudi.farkas@gmail.com>
2022-06-11 22:17:42 +0200

    chapter 2. add test structs and tests

--- FlightSchoolCodableTests/Chapter_2_Tests.swift
+++ FlightSchoolCodableTests/Chapter_2_Tests.swift
@@ -9,6 +9,48 @@
 import RudifaUtilPkg
 import XCTest
 
+struct Aircraft: Codable, Equatable {
+    var identification: String
+    var color: String
+}
+
+enum FlightRules: String, Codable, Equatable {
+    case visual = "VFR"
+    case instrument = "IFR"
+}
+
+struct FlightPlan: Codable, Equatable {
+    var aircraft: Aircraft
+    var route: [String]
+    var flightRules: FlightRules
+    private var departureDates: [String: Date]
+    var remarks: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case aircraft
+        case route
+        case flightRules = "flight_rules"
+        case departureDates = "departure_time"
+        case remarks
+    }
+
+    var proposedDepartureDate: Date? {
+        return departureDates["proposed"]
+    }
+
+    var actualDepartureDate: Date? {
+        return departureDates["actual"]
+    }
+
+    init(aircraft: Aircraft, route: [String], flightRules: FlightRules, departureDates: [String: Date], remarks: String?) {
+        self.aircraft = aircraft
+        self.route = route
+        self.flightRules = flightRules
+        self.departureDates = departureDates
+        self.remarks = remarks
+    }
+}
+
 class Chapter_2_Tests: XCTestCase {
     override func setUpWithError() throws {}
 
@@ -35,41 +77,48 @@ class Chapter_2_Tests: XCTestCase {
             let decoder = JSONDecoder()
             // decoder.keyDecodingStrategy = .convertFromSnakeCase
             decoder.dateDecodingStrategy = .iso8601
-        }
 
-        struct Aircraft: Codable, Equatable {
-            var identification: String
-            var color: String
-        }
+            do {
+                let plan = try decoder.decode(FlightPlan.self, from: jsonData)
+                printClassAndFunc("plan: \(plan)")
 
-        enum FlightRules: String, Codable, Equatable {
-            case visual = "VFR"
-            case instrument = "IFR"
-        }
+                let expected = FlightPlan(
+                    aircraft: Aircraft(
+                        identification: "NA12345",
+                        color: "Blue/White"
+                    ),
+                    route: ["KTTD", "KHIO"],
+                    flightRules: .visual,
+                    departureDates: [
+                        "proposed": Date(timeIntervalSince1970: 1524258900),
+                        "actual": Date(timeIntervalSince1970: 1524259200),
+                    ],
+                    remarks: nil
+                )
+                printClassAndFunc("expected: \(expected)")
 
-        struct FlightPlan: Codable, Equatable {
-            var aircraft: Aircraft
-            var route: [String]
-            var flightRules: FlightRules
-            private var departureDates: [String: Date]
-            var remarks: String?
+                XCTAssertEqual(plan, expected)
 
-            private enum CodingKeys: String, CodingKey {
-                case aircraft
-                case route
-                case flightRules = "flight_rules"
-                case departureDates = "departure_time"
-                case remarks
-            }
-
-
-            var proposedDepartureDate: Date? {
-                return departureDates["proposed"]
-            }
-
-            var actualDepartureDate: Date? {
-                return departureDates["actual"]
+            } catch {
+                printClassAndFunc("error: \(error)")
             }
         }
+
+        let newFormatter = ISO8601DateFormatter()
+        let date1 = newFormatter.date(from: "2018-04-20T14:15:00-07:00")!
+        printClassAndFunc("date1= \(date1) \(date1.timeIntervalSince1970) ")
+        let date2 = newFormatter.date(from: "2018-04-20T14:20:00-07:00")!
+        printClassAndFunc("date2= \(date2) \(date2.timeIntervalSince1970) ")
+
+//        "proposed": "2018-04-20T14:15:00-07:00",
+//        "actual": "2018-04-20T14:20:00-07:00"
+        // func to convert ISO8601 date string to Date timestamp
+        func iso8601Date(from string: String) -> TimeInterval {
+            let formatter = ISO8601DateFormatter()
+            return formatter.date(from: string)!.timeIntervalSince1970
+        }
+        let date22 = iso8601Date(from: "2018-04-20T14:20:00-07:00")
+        printClassAndFunc("date22=  \(date22) ")
+
     }
 }
